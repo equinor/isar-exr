45a6ca8433d6f86a67f9c7c39884728a704727e4
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-02-28 12:09:22 +0100

    Add GraphQL client

--- test.py
+++ test.py
@@ -1,38 +1,29 @@
-import json
-import requests
-from gql import Client, gql
-from gql.transport.aiohttp import AIOHTTPTransport
-from requests.auth import HTTPBasicAuth
-from isar_exr.config.settings import settings
+from time import sleep
+from graphql import GraphQLError
+from isar_exr.api.graphql_client import GraphqlClient
+from gql.transport.exceptions import TransportError
 
-username = settings.ROBOT_API_USERNAME
-password = settings.ROBOT_API_PASSWORD
-auth_url = settings.ROBOT_AUTH_URL
-r = requests.post(
-    auth_url,
-    auth=HTTPBasicAuth(username=username, password=password),
-)
-jsonrespons = json.loads(r.content.decode("utf-8"))
-token = jsonrespons["access_token"]
-reqHeaders = {
-    "authorization": "Bearer " + token,
-}
-transport = AIOHTTPTransport(url=settings.ROBOT_API_URL, headers=reqHeaders)
-
-# Create a GraphQL client using the defined transport
-client = Client(transport=transport, fetch_schema_from_transport=True)
+graph_client: GraphqlClient = GraphqlClient()
 
 # Provide a GraphQL query
-query = gql(
-    """
-    query robotFleet {
+query_string = """
+    query queryName {
       robotFleet {
         id
       }
     }
 """
-)
 
 # Execute the query on the transport
-result = client.execute(query)
-print(result)
+while True:
+    print()
+    try:
+        result = graph_client.query(query_string)
+        print()
+        print(result)
+    except GraphQLError as e:
+        print(f"Error during query: {e.message}")
+    except TransportError as e:
+        print(f"Error during communcation with server: {e}")
+
+    sleep(2)
