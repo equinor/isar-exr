493bff371584024a55b873543f9538b13cebf52e
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-03-01 14:41:17 +0100

    Add pause_current_mission to api

--- src/isar_exr/api/energy_robotics_api.py
+++ src/isar_exr/api/energy_robotics_api.py
@@ -1,8 +1,11 @@
-from isar_exr.api.graphql_client import GraphqlClient
-from isar_exr.models.step_status import ExrMissionStatus
+from typing import Any, Dict
+
+from robot_interface.models.exceptions import RobotException
 from robot_interface.models.mission.status import StepStatus
-from typing import Any
+
+from isar_exr.api.graphql_client import GraphqlClient
 from isar_exr.models.exceptions import NoMissionRunningException
+from isar_exr.models.step_status import ExrMissionStatus
 
 
 class Api:
@@ -39,3 +42,27 @@ class Api:
         response_dict: dict[str, Any] = self.client.query(query_string, params)
         is_running: bool = response_dict["isMissionRunning"]
         return is_running
+
+    def pause_current_mission(self, exr_robot_id: str) -> None:
+        query_string: str = """
+            mutation pauseMission($robot_id: String!)
+                {
+                    pauseMissionExecution(robotID:$robot_id)
+                    {
+                        id, status, failures
+                    }
+                }
+        """
+        params: dict = {"robot_id": exr_robot_id}
+        try:
+            result: Dict[str, Any] = self.client.query(query_string, params)
+        except Exception as e:
+            raise RobotException(e)
+
+        status: ExrMissionStatus = ExrMissionStatus(result["status"])
+        success: bool = status in [
+            ExrMissionStatus.Paused,
+            ExrMissionStatus.PauseRequested,
+        ]
+        if not success:
+            raise RobotException(f"Invalid status after pausing mission: '{status}'")
