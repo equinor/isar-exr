6fb13049c1cc8480834b33f8f8bb3fa8556f1735
eivindsjovold <esjo@equinor.com>
2023-03-01 09:47:18 +0100

    Add check for isMissionRunning

--- src/isar_exr/api/energy_robotics_api.py
+++ src/isar_exr/api/energy_robotics_api.py
@@ -4,25 +4,42 @@ from isar_exr.api.graphql_client import GraphqlClient
 from isar_exr.models.step_status import ExrMissionStatus
 from robot_interface.models.mission.status import StepStatus
 from typing import Any
+from isar_exr.models.exceptions import NoMissionRunningException
 
 
-
-class Api():
+class Api:
     def __init__(self):
         self.client = GraphqlClient()
 
     def get_step_status(self, exr_robot_id: str) -> StepStatus:
-        query_string: str = '''
+        query_string: str = """
             query currentMissionExecution($robot_id: String!) {
                 currentMissionExecution(robotID: $robot_id) {
                     status
                     }
                 }
-        '''
+        """
         params: dict = {"robot_id": exr_robot_id}
         document: DocumentNode = gql(query_string)
-        #this returns null if there is no mission being run
+        if not self.is_mission_running(exr_robot_id):
+            raise NoMissionRunningException(
+                f"Cannot get EXR mission status - No EXR mission is running for robot with id {exr_robot_id}"
+            )
+
         response_dict: dict[str, Any] = self.client.query(document, params)
-        print(response_dict)
-        step_status = ExrMissionStatus(response_dict["currentMissionExecution"]["status"])
+        step_status = ExrMissionStatus(
+            response_dict["currentMissionExecution"]["status"]
+        )
         return step_status.to_step_status()
+
+    def is_mission_running(self, exr_robot_id: str):
+        query_string: str = """
+            query isMissionRunning($robot_id: String!) {
+                isMissionRunning(robotID: $robot_id)
+                }
+        """
+        params: dict = {"robot_id": exr_robot_id}
+        document: DocumentNode = gql(query_string)
+        response_dict: dict[str, Any] = self.client.query(document, params)
+        is_running: bool = response_dict["isMissionRunning"]
+        return is_running
