d8caf21e14c7e8f99260e29245025f5d74285893
Torstein Lundervold Nesheim <TLNE@equinor.com>
2023-03-02 10:46:43 +0100

    Mock GraphQL client in test

--- tests/interfaces/test_robotinterface.py
+++ tests/interfaces/test_robotinterface.py
@@ -1,7 +1,11 @@
+from unittest import mock
+
 from robot_interface.test_robot_interface import interface_test
 
+from isar_exr.api.graphql_client import GraphqlClient
 from isar_exr.robotinterface import ExrRobot
 
 
+@mock.patch.object(GraphqlClient, "__init__", mock.Mock(return_value=None))
 def test_robotinterface():
     interface_test(ExrRobot())
