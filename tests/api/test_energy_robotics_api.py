677eccf6666b7d02c2cdf849e9a58b3db9f6a159
Afonso <avie@equinor.com>
2023-03-06 10:38:09 +0100

    Add tests for task creator functions

--- tests/api/test_energy_robotics_api.py
+++ tests/api/test_energy_robotics_api.py
@@ -1,10 +1,12 @@
 from typing import Any, Dict
 from unittest import mock
+from isar_exr.api.graphql_client import GraphqlClient
 
 import pytest
 from gql import Client
 
 from isar_exr.api.energy_robotics_api import EnergyRoboticsApi
+from robot_interface.models.exceptions import RobotException
 
 
 @mock.patch(
@@ -33,3 +35,104 @@ class TestPauseMission:
         api: EnergyRoboticsApi = EnergyRoboticsApi()
         with pytest.raises(expected_exception=Exception):
             api.pause_current_mission("test_exr_robot_id")
+
+
+@mock.patch(
+    "isar_exr.api.graphql_client.get_access_token",
+    mock.Mock(return_value="test_token"),
+)
+class TestTaskCreatorFunctions:
+    site_id = "Costa Nova"
+    task_name = "dummy task"
+    docking_station_id = "dummy docking station id"
+    point_of_interest_id = "dummy poi id"
+    pose_3d_stamped_input = mock.Mock()
+
+    dock_robot_mocked_response: Dict[str, Any] = {
+        "createDockRobotTaskDefinition": {"id": "dummy_task_id"}
+    }
+    point_of_interest_inspection_mocked_response: Dict[str, Any] = {
+        "createPoiInspectionTaskDefinition": {"id": "dummy_task_id"}
+    }
+    waypoint_mocked_response: Dict[str, Any] = {
+        "createWaypointTaskDefinition": {"id": "dummy_task_id"}
+    }
+
+    @mock.patch.object(
+        GraphqlClient, "query", mock.Mock(return_value=dock_robot_mocked_response)
+    )
+    def test_create_dock_robot_task_definition_success(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        received_task_id: str = api.create_dock_robot_task_definition(
+            site_id=self.site_id,
+            task_name=self.task_name,
+            docking_station_id=self.docking_station_id,
+        )
+        assert (
+            received_task_id
+            == self.dock_robot_mocked_response["createDockRobotTaskDefinition"]["id"]
+        )
+
+    @mock.patch.object(GraphqlClient, "query", mock.Mock(side_effect=Exception()))
+    def test_create_dock_robot_task_definition_error(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        with pytest.raises(expected_exception=RobotException):
+            api.create_dock_robot_task_definition(
+                site_id=self.site_id,
+                task_name=self.task_name,
+                docking_station_id=self.docking_station_id,
+            )
+
+    @mock.patch.object(
+        GraphqlClient,
+        "query",
+        mock.Mock(return_value=point_of_interest_inspection_mocked_response),
+    )
+    def test_create_point_of_interest_inspection_task_definition_success(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        received_task_id: str = api.create_point_of_interest_inspection_task_definition(
+            site_id=self.site_id,
+            task_name=self.task_name,
+            point_of_interest_id=self.point_of_interest_id,
+        )
+        assert (
+            received_task_id
+            == self.point_of_interest_inspection_mocked_response[
+                "createPoiInspectionTaskDefinition"
+            ]["id"]
+        )
+
+    @mock.patch.object(GraphqlClient, "query", mock.Mock(side_effect=Exception()))
+    def test_create_point_of_interest_inspection_task_definition_error(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        with pytest.raises(expected_exception=RobotException):
+            api.create_point_of_interest_inspection_task_definition(
+                site_id=self.site_id,
+                task_name=self.task_name,
+                point_of_interest_id=self.point_of_interest_id,
+            )
+
+    @mock.patch.object(
+        GraphqlClient, "query", mock.Mock(return_value=waypoint_mocked_response)
+    )
+    def test_create_waypoint_task_definition_success(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        received_task_id: str = api.create_waypoint_task_definition(
+            site_id=self.site_id,
+            task_name=self.task_name,
+            pose_3D_stamped_input=self.pose_3d_stamped_input,
+        )
+        assert (
+            received_task_id
+            == self.waypoint_mocked_response["createWaypointTaskDefinition"]["id"]
+        )
+
+    @mock.patch.object(GraphqlClient, "query", mock.Mock(side_effect=Exception()))
+    def test_create_waypoint_task_definition_error(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        with pytest.raises(expected_exception=RobotException):
+            api.create_waypoint_task_definition(
+                site_id=self.site_id,
+                task_name=self.task_name,
+                pose_3D_stamped_input=self.pose_3d_stamped_input,
+            )
