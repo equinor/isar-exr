4dfa9d96fc8b6eeb7925d3c9376343a4d5f75c5a
Morten Fredriksen <morfre@equinor.com>
2023-03-07 16:15:11 +0100

    Add tests to initialize

--- tests/api/test_energy_robotics_api.py
+++ tests/api/test_energy_robotics_api.py
@@ -1,12 +1,15 @@
+from datetime import datetime, timedelta
 from typing import Any, Dict
 from unittest import mock
 from isar_exr.api.graphql_client import GraphqlClient
+from isar_exr.api.models.enums import AwakeStatus
 
 import pytest
 from gql import Client
 
 from isar_exr.api.energy_robotics_api import EnergyRoboticsApi
 from robot_interface.models.exceptions import RobotException
+from isar_exr.config.settings import settings
 
 
 @mock.patch(
@@ -136,3 +139,69 @@ class TestTaskCreatorFunctions:
                 task_name=self.task_name,
                 pose_3D_stamped_input=self.pose_3d_stamped_input,
             )
+
+
+@mock.patch(
+    "isar_exr.api.graphql_client.get_access_token",
+    mock.Mock(return_value="test_token"),
+)
+class TestWakeUpRobot:
+    wake_up_requested_response: Dict[str, Any] = {"id": "command_id"}
+
+    @mock.patch.object(
+        Client, "execute", mock.Mock(return_value=wake_up_requested_response)
+    )
+    @mock.patch.object(
+        EnergyRoboticsApi, "is_robot_awake", mock.Mock(return_value=True)
+    )
+    def test_succeeds_if_robot_is_awake(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        api.wake_up_robot("test_exr_robot_id")
+
+    @mock.patch.object(
+        Client, "execute", mock.Mock(return_value=wake_up_requested_response)
+    )
+    @mock.patch.object(
+        EnergyRoboticsApi, "is_robot_awake", mock.Mock(return_value=False)
+    )
+    def test_fails_if_robot_is_not_awake_after_max_time(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        with pytest.raises(expected_exception=RobotException):
+            api.wake_up_robot("test_exr_robot_id", 1)
+
+
+@mock.patch(
+    "isar_exr.api.graphql_client.get_access_token",
+    mock.Mock(return_value="test_token"),
+)
+class TestRobotAwakeQuery:
+    awake_status_awake: Dict[str, Any] = {"awakeStatus": AwakeStatus.Awake}
+    awake_status_waking_up: Dict[str, Any] = {"awakeStatus": AwakeStatus.WakingUp}
+    awake_status_asleep: Dict[str, Any] = {"awakeStatus": AwakeStatus.Asleep}
+    awake_status_going_to_sleep: Dict[str, Any] = {
+        "awakeStatus": AwakeStatus.GoingToSleep
+    }
+
+    @mock.patch.object(Client, "execute", mock.Mock(return_value=awake_status_awake))
+    def test_returns_true_if_robot_is_awake(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        assert api.is_robot_awake("test_exr_robot_id") == True
+
+    @mock.patch.object(
+        Client, "execute", mock.Mock(return_value=awake_status_waking_up)
+    )
+    def test_returns_false_if_robot_is_waking_up(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        assert api.is_robot_awake("test_exr_robot_id") == False
+
+    @mock.patch.object(Client, "execute", mock.Mock(return_value=awake_status_asleep))
+    def test_returns_false_if_robot_is_asleep(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        assert api.is_robot_awake("test_exr_robot_id") == False
+
+    @mock.patch.object(
+        Client, "execute", mock.Mock(return_value=awake_status_going_to_sleep)
+    )
+    def test_returns_false_if_robot_is_going_toawake_status_going_to_sleep(self):
+        api: EnergyRoboticsApi = EnergyRoboticsApi()
+        assert api.is_robot_awake("test_exr_robot_id") == False
